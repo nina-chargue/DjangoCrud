77a31f03d03871486a91216ac31ffbace2968e84
nina-chargue <nina8chargue@gmail.com>
2023-09-04 22:18:02 -0600

    Testing changes

--- djangocrud/deployment.py
+++ djangocrud/deployment.py
@@ -7,7 +7,7 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
 # ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-ALLOWED_HOSTS = ['https://mycodecave.azurewebsites.net/']
+ALLOWED_HOSTS = ['https://mycodecave.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
